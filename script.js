1e7ee2aee7a44837d1f1359ee4c1b70d720d7bf0
mflojo <m.eric.flojo@gmail.com>
2025-01-29 15:49:13 -0800

    Add testing for script exec pageload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector("#container");
+    container.textContent = "Testing page load";
+});
+
+
+let pixel = 16;
+
+// canvas.addEventListener("load", () => {
+//     for (let i=0; i<16; i++) {
+//         for (let u=0; u<16; u++) {
+
+//         }
+//     }
+// });
