fdf8d400fcb1944d207ea24bdd2e3536c9f0f887
mflojo <m.eric.flojo@gmail.com>
2025-01-29 21:07:07 -0800

    Add event to create grid on pageload

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector("#container");
-    container.textContent = "Testing page load";
+    const body = document.querySelector("body");
+    const gridSize = 16;
+    const boxSize = `calc(100% / ${gridSize})`;
+
+    body.style.display = "flex";
+    body.style.justifyContent = "center";
+    body.style.alignItems = "center"
+    body.style.height = "100vh";
+    body.style.margin = "0";
+    container.style.display = "flex";
+    container.style.flexWrap = "wrap";
+    container.style.width = "800px";
+    container.style.height = "800px"
+    container.style.justifyContent = "center";
+    container.style.alignItems = "center"
+
+    for (let i=0; i<gridSize*gridSize; i++) {
+        const box = document.createElement("div");
+        box.style.width = boxSize;
+        box.style.height = boxSize;
+        box.style.border = "1px solid black";
+        box.style.boxSizing = "border-box";
+        container.appendChild(box);
+    }
 });
-
-
-let pixel = 16;
-
-// canvas.addEventListener("load", () => {
-//     for (let i=0; i<16; i++) {
-//         for (let u=0; u<16; u++) {
-
-//         }
-//     }
-// });
