bda067031144090bf3e00f8072bc0e49966fd0f7
mflojo <m.eric.flojo@gmail.com>
2025-01-29 21:57:41 -0800

    Add button for user input size the grid

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector("#container");
+    const button = document.querySelector("#button");
     const body = document.querySelector("body");
-    const gridSize = 16;
-    const boxSize = `calc(100% / ${gridSize})`;
+    const maxSize = 100;
+    const containerSize = 800
 
-    body.style.display = "flex";
-    body.style.justifyContent = "center";
-    body.style.alignItems = "center"
-    body.style.height = "100vh";
-    body.style.margin = "0";
-    container.style.display = "flex";
-    container.style.flexWrap = "wrap";
-    container.style.width = "800px";
-    container.style.height = "800px"
-    container.style.justifyContent = "center";
-    container.style.alignItems = "center"
+    function createGrid(gridSize) {
+        container.innerHTML = "";
 
-    for (let i=0; i<gridSize*gridSize; i++) {
-        const box = document.createElement("div");
-        box.style.width = boxSize;
-        box.style.height = boxSize;
-        box.style.border = "1px solid black";
-        box.style.boxSizing = "border-box";
-        container.appendChild(box);
+        body.style.display = "flex";
+        body.style.flexDirection = "column";
+        body.style.gap = "10px";
+        body.style.justifyContent = "center";
+        body.style.alignItems = "center"
+        body.style.height = "100vh";
+        body.style.margin = "0";
 
-        box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "blue";
-        })
+        container.style.display = "flex";
+        container.style.flexWrap = "wrap";
+        container.style.width = `${containerSize}px`;
+        container.style.height = `${containerSize}px`;
+        container.style.justifyContent = "center";
+        container.style.alignItems = "center";
+
+        const boxSize = `${containerSize / gridSize}px`;
+
+        for (let i=0; i<gridSize*gridSize; i++) {
+            const box = document.createElement("div");
+            box.style.width = boxSize;
+            box.style.height = boxSize;
+            box.style.border = "1px solid black";
+            box.style.boxSizing = "border-box";
+    
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "blue";
+            })
+    
+            container.appendChild(box);
+        }
     }
+
+    button.addEventListener("click", () => {
+        let newSize = prompt("Enter new grid size (1-100 only):", "16");
+
+        if (newSize !== null) {
+            newSize = parseInt(newSize);
+
+            if (isNaN(newSize) || newSize < 1 || newSize > maxSize) {
+                alert("Grid can only be from 1-100 in size.");
+            } else {
+                createGrid(newSize);
+            }
+        }
+    })
+
+    createGrid(16);
+   
 });
